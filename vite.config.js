a36ec8a11557344168c319d036c372f0ccf881b1
dmjDev <dmj.develop@gmail.com>
2026-01-03 13:36:42 +0100

    Update 13:36 3/1/26

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dmjdev-buscador-peliculas/',
+  base: '/dmjdev-react-buscador-peliculas/',
 })
